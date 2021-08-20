b38a464eb3d2c3045402b7f4e9f4bc6130d5e11a
Jean-PierrePapin <skouo82@gmail.com>
2021-08-20 14:02:18 +0100

    Actually counting the words, characters and read time

--- our-first-unique-plugin/our-fisrt-unique-plugin.php
+++ our-first-unique-plugin/our-fisrt-unique-plugin.php
@@ -9,11 +9,65 @@
 */
 
 class WordCountAndTimePlugin {
+	/**
+	 * Constructor function of the class WordCountAndTimePlugin
+	 */
 	function __construct() {
 		add_action('admin_menu', [$this, 'adminPage']);
 		add_action('admin_init', [$this, 'settings']);
+		add_filter('the_content', [$this, 'ifWrap']);
 	}
 
+	/**
+	 * Function to check if the page is a single post, the query is the main query and the plugin checkboxes are all checked
+	 */
+	function ifWrap($content) {
+		if (is_main_query() AND is_single() AND 
+			(
+				get_option('wcp_wordcount', '1') OR 
+				get_option('wcp_charactercount', '1') OR 
+				get_option('wcp_readtime', '1')
+			)
+		) {
+			return $this->createHTML($content);
+		}
+		return $content;
+	}
+
+	/**
+	 * Function to add the content at the beginning of a single post
+	 */
+	function createHTML($content) {
+		$html = '<h3>' . esc_html(get_option('wcp_headline', 'Post Statistics')) . '</h3><p>';
+
+		// get word count once because both wordcount and read time will need it
+		if (get_option('wcp_wordcount', '1') OR get_option('wcp_readtime', '1')) {
+			$wordCount = str_word_count(strip_tags($content));
+		}
+
+		if (get_option('wcp_wordcount', '1')) {
+			$html .= 'This post has ' . $wordCount . ' words.<br>';
+		}
+
+		if (get_option('wcp_charactercount', '1')) {
+			$html .= 'This post has ' . strlen(strip_tags($content)) . ' characters.<br>';
+		}
+
+		if (get_option('wcp_readtime', '1')) {
+			$html .= 'This post will take about ' . count([$wordCount/225]) . ' minute(s) to read.<br>';
+		}
+
+		$html .= '</p>';
+
+		if (get_option('wcp_location', '0')  == '0') {
+			return $html . $content;
+		}
+		return $content . $html;
+	}
+
+	/**
+	 * Function for the plugin settings
+	 */
 	function settings() {
 		add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
 		
@@ -38,6 +92,9 @@ class WordCountAndTimePlugin {
 		register_setting('wordcountplugin', 'wcp_readtime', ['sanitize_callback' => 'sanitize_text_field', 'default' => '1']);
 	}
 
+	/**
+	 * Function to sanitize the value sent to the database to be equal to 0 or 1
+	 */
 	function sanitizeLocation($input) {
 		if ($input != '0' AND $input != '1') {
 			add_settings_error('wcp_location', 'wcp_location_error', 'Display location must be either beginning or end!');
@@ -76,6 +133,9 @@ class WordCountAndTimePlugin {
 		add_options_page('Word Count Setting', 'Word Count', 'manage_options', 'word-count-settings-page', [$this, 'ourHTML']);
 	}
 	
+	/**
+	 * Function to display the HTML codes in the admin section in the WP admin backend
+	 */
 	function ourHTML() { ?>
 		<div class="wrap">
 			<h1>Word Count Settings</h1>
@@ -91,6 +151,9 @@ class WordCountAndTimePlugin {
 	<?php }
 }
 
+/**
+ * Variable to instantiate the class WordCountAndTimePlugin in order to manipulate it
+ */
 $wordCountAndTimePlugin = new WordCountAndTimePlugin();
 
 
