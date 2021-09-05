ce9773dbaac39cd95e9310c58d378275cda52f40
Jean-PierrePapin <skouo82@gmail.com>
2021-09-05 18:53:36 +0100

    Letting users click on an answer

--- are-you-paying-attention/build/frontend.asset.php
+++ are-you-paying-attention/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '327503caace98d6419a6435b5b27e3c5');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => 'ddbd5886c869360d1787d0e4eb824c02');
